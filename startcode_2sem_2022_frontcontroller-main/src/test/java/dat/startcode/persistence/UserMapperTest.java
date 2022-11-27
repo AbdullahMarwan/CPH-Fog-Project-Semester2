556ab196e39d322a26b5b37349c995a38e10860b
AbdullahMarwan <abdu.moussa01@gmail.com>
2022-11-27 20:57:38 +0100

    Added starcode

---
+++ startcode_2sem_2022_frontcontroller-main/src/test/java/dat/startcode/persistence/UserMapperTest.java
@@ -0,0 +1,88 @@
+package dat.startcode.persistence;
+
+import dat.startcode.model.entities.User;
+import dat.startcode.model.exceptions.DatabaseException;
+import dat.startcode.model.persistence.ConnectionPool;
+import dat.startcode.model.persistence.Facade;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserMapperTest
+{
+    private final static String USER = "root";
+    private final static String PASSWORD = "root";
+    private final static String URL = "jdbc:mysql://localhost:3306/startcode_test?serverTimezone=CET&allowPublicKeyRetrieval=true&useSSL=false";
+
+    private static ConnectionPool connectionPool;
+
+    @BeforeAll
+    public static void setUpClass() {
+            connectionPool = new ConnectionPool(USER, PASSWORD, URL);
+    }
+
+    @BeforeEach
+    void setUp()
+    {
+        try (Connection testConnection = connectionPool.getConnection()) {
+            try (Statement stmt = testConnection.createStatement() ) {
+                // Remove all rows from all tables
+                stmt.execute("delete from user");
+                // Indsæt et par brugere
+                stmt.execute("insert into user (username, password, role) " +
+                        "values ('user','1234','user'),('admin','1234','admin'), ('ben','1234','user')");
+            }
+        } catch (SQLException throwables) {
+            System.out.println(throwables.getMessage());
+            fail("Database connection failed");
+        }
+    }
+
+    @Test
+    void testConnection() throws SQLException
+    {
+        Connection connection = connectionPool.getConnection();
+        assertNotNull(connection);
+        if (connection != null)
+        {
+            connection.close();
+        }
+    }
+
+    @Test
+    void login() throws DatabaseException
+    {
+        User expectedUser = new User("user","1234","user");
+        User actualUser = Facade.login("user","1234", connectionPool);
+        assertEquals(expectedUser, actualUser);
+    }
+
+    @Test
+    void invalidPasswordLogin() throws DatabaseException
+    {
+        assertThrows(DatabaseException.class, () -> Facade.login("user","123", connectionPool));
+    }
+
+    @Test
+    void invalidUserNameLogin() throws DatabaseException
+    {
+        assertThrows(DatabaseException.class, () -> Facade.login("bob","1234", connectionPool));
+    }
+
+    @Test
+    void createUser() throws DatabaseException
+    {
+        User newUser = Facade.createUser("jill", "1234", "user", connectionPool);
+        User logInUser = Facade.login("jill","1234", connectionPool);
+        User expectedUser = new User("jill", "1234", "user");
+        assertEquals(expectedUser, newUser);
+        assertEquals(expectedUser, logInUser);
+
+    }
+}
