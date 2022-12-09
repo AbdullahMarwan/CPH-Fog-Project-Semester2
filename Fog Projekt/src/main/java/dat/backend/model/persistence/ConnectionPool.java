5b55c202213d75b2b36cff4ad284c0a388bf3ac0
AbdullahMarwan <abdu.moussa01@gmail.com>
2022-12-09 20:52:56 +0100

    Fixed startup of site

--- Fog Projekt/src/main/java/dat/backend/model/persistence/ConnectionPool.java
+++ Fog Projekt/src/main/java/dat/backend/model/persistence/ConnectionPool.java
@@ -13,9 +13,9 @@ public class ConnectionPool
     // TODO: Change access credentials for MySql server as needed below:
 
     private HikariDataSource ds;
-    private static String USER = "root";
-    private static String PASSWORD = "root";
-    private static String URL = "jdbc:mysql://localhost:3306/startcode";
+    private static String USER = "fog";
+    private static String PASSWORD = "1234";
+    private static String URL = "jdbc:mysql://localhost:3306/fog";
 
     public ConnectionPool()
     {
