8e1a4bb2912bc63b9ebd65198e4382f841038b30
AbdullahMarwan <abdu.moussa01@gmail.com>
2022-12-12 14:47:29 +0100

    Fixed Database Connection and Sign Up page creates User

---
+++ Fog Projekt/src/main/java/dat/backend/control/Signup.java
@@ -0,0 +1,50 @@
+package dat.backend.control;
+
+import dat.backend.model.config.ApplicationStart;
+import dat.backend.model.entities.User;
+import dat.backend.model.exceptions.DatabaseException;
+import dat.backend.model.persistence.ConnectionPool;
+import dat.backend.model.persistence.UserFacade;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "Signup", value = "/signup")
+public class Signup extends HttpServlet {
+    private ConnectionPool connectionPool;
+
+    @Override
+    public void init() throws ServletException
+    {
+        this.connectionPool = ApplicationStart.getConnectionPool();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        HttpSession session = request.getSession();
+        session.setAttribute("user", null); // invalidating user object in session scope
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+            User user = UserFacade.createUser(username, password, "user", connectionPool);
+
+            session = request.getSession();
+            session.setAttribute("user", user); // adding user object to session scope
+
+            request.getRequestDispatcher("WEB-INF/usercreation.jsp").forward(request, response);
+        } catch (DatabaseException e) {
+            request.setAttribute("errormessage", e.getMessage());
+            request.getRequestDispatcher("error.jsp").forward(request, response);
+        }
+    }
+}
