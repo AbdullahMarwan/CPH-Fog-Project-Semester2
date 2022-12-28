15d7bc79c5da55dbd4bdd8bb18da42e2160d6198
AbdullahMarwan <abdu.moussa01@gmail.com>
2022-12-28 23:15:44 +0100

    Added ability to override based on env values

--- Fog Projekt/src/main/java/dat/backend/model/persistence/PersistenceManager.java
+++ Fog Projekt/src/main/java/dat/backend/model/persistence/PersistenceManager.java
@@ -1,19 +1,34 @@
 package dat.backend.model.persistence;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PersistenceManager {
-    EntityManager entityManager;
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
 
     public PersistenceManager(String entityUnit) {
-        this.entityManager = Persistence
-                .createEntityManagerFactory(entityUnit)
-                .createEntityManager();
+        if (System.getenv("DEPLOYED") != null) {
+            Map overridenProperties = new HashMap<>();
+
+            overridenProperties.put("javax.persistence.jdbc.url", System.getenv("JDBC_HOST") + System.getenv("JDBC_DB") + System.getenv("JDBC_PARAMS"));
+            overridenProperties.put("javax.persistence.jdbc.user", System.getenv("JDBC_USER"));
+            overridenProperties.put("javax.persistence.jdbc.password", System.getenv("JDBC_PASSWORD"));
+
+            this.entityManagerFactory = Persistence.createEntityManagerFactory(entityUnit, overridenProperties);
+            this.entityManager = this.entityManagerFactory.createEntityManager();
+        } else {
+            this.entityManagerFactory = Persistence.createEntityManagerFactory(entityUnit);
+            this.entityManager = this.entityManagerFactory.createEntityManager();
+        }
     }
 
-    public EntityTransaction entityTransaction(){
+    public EntityTransaction entityTransaction() {
         return this.entityManager.getTransaction();
     }
 
