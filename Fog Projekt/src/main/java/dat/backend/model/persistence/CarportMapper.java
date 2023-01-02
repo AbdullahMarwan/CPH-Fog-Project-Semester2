cbb06713d83d407b1972b3c7336a5958c22793ec
AbdullahMarwan <abdu.moussa01@gmail.com>
2023-01-02 19:31:48 +0100

    Cleaned up code

--- Fog Projekt/src/main/java/dat/backend/model/persistence/CarportMapper.java
+++ Fog Projekt/src/main/java/dat/backend/model/persistence/CarportMapper.java
@@ -16,43 +16,10 @@ public class CarportMapper {
     static List<Material> getMaterials() {
         Logger.getLogger("web").log(Level.INFO, "");
 
-        PersistenceManager persistenceManager = new PersistenceManager("fog-user-unit");
+        PersistenceManager persistenceManager = new PersistenceManager("fog-unit");
 
         TypedQuery<Material> query = persistenceManager.getEntityManager().createNamedQuery("getAllMaterials", Material.class);
 
         return query.getResultList();
     }
-
-
-    /*
-    static List<Material> getMaterials(dat.backend.model.persistence.ConnectionPool connectionPool) {
-        List<Material> materialList = new ArrayList<>();
-
-        String sql = "select * from materials";
-
-        try (Connection connection = connectionPool.getConnection()) {
-            try (PreparedStatement ps = connection.prepareStatement(sql)) {
-                ResultSet rs = ps.executeQuery();
-                while (rs.next()) {
-                    int id = rs.getInt("material_id");
-                    String name = rs.getString("name");
-                    int price = rs.getInt("price");
-                    int unit_id = rs.getInt("unit_id");
-                    String description = rs.getString("description");
-                    int length = rs.getInt("length");
-
-                    Material newMaterial = new Material(id, name, price, unit_id, description, length);
-                    materialList.add(newMaterial);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return materialList;
-    }
-
-     */
-
 }
